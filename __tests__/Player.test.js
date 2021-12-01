3ed813d61f6f3e477a405c81a544e91a9ff72c86
Sihayah <sihaya_a@hotmail.com>
2021-12-01 04:55:17 -0500

    refactor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require('../lib/Potion')
 
 jest.mock('../lib/Potion')
 
-console.log(new Potion())
-
 test('creates a player object', () => {
     const player = new Player('notDave')
 
@@ -14,4 +12,21 @@ test('creates a player object', () => {
     expect(player.strength).toEqual(expect.any(Number))
     expect(player.agility).toEqual(expect.any(Number))
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]))
+})
+
+test("gets player's stats as an object", () => {
+    const player = new Player('notDave')
+
+    expect(player.getStats()).toHaveProperty('potions')
+    expect(player.getStats()).toHaveProperty('health')
+    expect(player.getStats()).toHaveProperty('strength')
+    expect(player.getStats()).toHaveProperty('agility')
+})
+
+test('gets inventory from the player or returns false', () => {
+    const player = new Player('notDave')
+
+    expect(player.getInventory()).toEqual(expect.any(Array))
+    player.inventory = []
+    expect(player.getInventory()).toEqual(false)
 })
